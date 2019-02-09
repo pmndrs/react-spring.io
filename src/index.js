a92527a3876779580f5677ac5ae916820d441836
Mihai Cernusca <mcernusca@gmail.com>
2019-02-08 22:15:33 -0500

    Add scroll restoration

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import IntersectionObserver from 'intersection-observer' // eslint-disable-line no-unused-vars
+import ManageScroll from './utils/manage-scroll'
 import App from './app'
 import './css/reset.css'
 import './css/styles.css'
 import * as serviceWorker from './other/service-worker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <>
+    <ManageScroll />
+    <App />
+  </>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
