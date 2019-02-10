57117cf7741087d908f812dcf3b297881e7cd26e
Mihai Cernusca <mcernusca@gmail.com>
2019-02-10 12:05:39 -0500

    Push page content down 20px

--- src/common/nav-page.js
+++ src/common/nav-page.js
@@ -1,8 +1,17 @@
 import React, {useEffect} from 'react'
+import styled from 'styled-components'
 
 export default function NavPage({currentPath, children}) {
   useEffect(() => {
     if (window.Prism) window.Prism.highlightAll()
   }, [])
-  return <div className="markdown-body">{children}</div>
+  return (
+    <Wrapper>
+      <div className="markdown-body">{children}</div>
+    </Wrapper>
+  )
 }
+
+export const Wrapper = styled.div`
+  padding-top: 20px;
+`
