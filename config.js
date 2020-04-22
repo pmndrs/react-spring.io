9770016ee96aacc8c717de2b8dc8e1c160601152
Alec Larson <alec.stanford.larson@gmail.com>
2020-04-22 10:00:38 -0400

    init

---
+++ config.js
@@ -0,0 +1,68 @@
+const config = {
+  gatsby: {
+    pathPrefix: '/',
+    siteUrl: 'https://react-spring.io',
+    gaTrackingId: null,
+    trailingSlash: false,
+  },
+  header: {
+    logo: '',
+    logoLink: '',
+    title: '',
+    githubUrl: 'https://github.com/react-spring/react-spring.io',
+    helpUrl: '',
+    tweetText: '',
+    social: '',
+    links: [{ text: '', link: '' }],
+    search: {
+      enabled: false,
+      indexName: '',
+      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
+      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
+    },
+  },
+  sidebar: {
+    forcedNavOrder: [
+      '/introduction', // add trailing slash if enabled above
+      '/codeblock',
+    ],
+    collapsedNav: [
+      '/codeblock', // add trailing slash if enabled above
+    ],
+    links: [],
+    frontline: false,
+    ignoreIndex: true,
+    title: '',
+  },
+  siteMetadata: {
+    title: 'React Spring',
+    description:
+      'Bring your components to life with simple spring animation primitives for React',
+    ogImage: 'https://www.react-spring.io/share.jpg',
+    docsLocation:
+      'https://github.com/react-spring/react-spring.io/tree/master/content',
+    favicon: '',
+  },
+  pwa: {
+    enabled: false, // disabling this will also remove the existing service worker.
+    manifest: {
+      name: '',
+      short_name: '',
+      start_url: '/',
+      background_color: '#6b37bf',
+      theme_color: '#6b37bf',
+      display: 'standalone',
+      crossOrigin: 'use-credentials',
+      icons: [
+        {
+          src: 'src/pwa-512.png',
+          sizes: `512x512`,
+          type: `image/png`,
+        },
+      ],
+    },
+  },
+}
+
+module.exports = config
