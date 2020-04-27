96fdea5c4a12934d85dffd3a35966ca3ec36f959
Alec Larson <alec.stanford.larson@gmail.com>
2020-04-27 09:25:26 -0400

    feat: make the LiveProvider more magical
    The wrapper code for making examples work is injected at runtime, using Nebu.
    
    The example animations are cancelled when off-screen, and they reset when they reappear on screen.
    
    Examples that throw (even when being edited) result in nothing being rendered. Much better than crashing the entire page!

--- src/components/mdxComponents/LiveProvider.js
+++ src/components/mdxComponents/LiveProvider.js
@@ -1,9 +1,67 @@
 import * as React from 'react'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import { a, useSpring } from 'react-spring'
+import VisibilitySensor from 'react-visibility-sensor'
+import nebu from 'nebu'
 
-const ReactLiveProvider = ({ code }) => {
+/** Use a value from the previous render */
+export function usePrev(value) {
+  const prevRef = React.useRef()
+  React.useEffect(() => {
+    prevRef.current = value
+  })
+  return prevRef.current
+}
+
+const scope = {
+  // Create and render a component that uses the example code.
+  __r: useExample => {
+    const Example = ({ visible }) => {
+      const wasVisible = usePrev(visible)
+      try {
+        const style = useExample({
+          reset: !wasVisible && visible,
+          cancel: !visible,
+        })
+        return (
+          <a.div
+            style={{
+              width: 80,
+              height: 80,
+              marginTop: 20,
+              backgroundColor: 'purple',
+              borderRadius: 16,
+              ...style,
+            }}
+          />
+        )
+      } catch (e) {
+        return null
+      }
+    }
+    return (
+      <VisibilitySensor>
+        {props => <Example visible={props.isVisible} />}
+      </VisibilitySensor>
+    )
+  },
+  useSpring,
+}
+
+const buble = {
+  transforms: {
+    arrow: false,
+    asyncAwait: false,
+  },
+}
+
+const ReactLiveProvider = ({ code, type }) => {
   return (
-    <LiveProvider code={code}>
+    <LiveProvider
+      code={code.trim()}
+      scope={scope}
+      buble={buble}
+      transformCode={transform}>
       <LiveEditor />
       <LiveError />
       <LivePreview />
@@ -12,3 +70,38 @@ const ReactLiveProvider = ({ code }) => {
 }
 
 export default ReactLiveProvider
+
+const hookIds = ['useSpring']
+
+function transform(input) {
+  const plugins = [
+    {
+      Program(node) {
+        const lastStmt = node.body[node.body.length - 1]
+        if (lastStmt && lastStmt.type !== 'ReturnStatement') {
+          if (lastStmt.type == 'ExpressionStatement') {
+            const expr = lastStmt.expression
+            if (
+              expr.type == 'CallExpression' &&
+              hookIds.indexOf(expr.callee.name) >= 0
+            ) {
+              const objExpr = expr.arguments.find(
+                e => e.type == 'ObjectExpression'
+              )
+              if (objExpr) {
+                objExpr.unshift('properties', '...props,\n')
+              }
+            }
+            expr.before('return ')
+          }
+        }
+      },
+    },
+  ]
+  try {
+    const output = nebu.process(input, { plugins })
+    return `__r(props => {\n${output}\n})`
+  } catch (e) {
+    return 'null'
+  }
+}
