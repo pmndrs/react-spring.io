03cef8b123725693b6ad800293fe9ffab31cf29c
Alec Larson <alec.stanford.larson@gmail.com>
2020-04-28 08:48:08 -0400

    nit: export the base theme

--- src/components/theme/index.js
+++ src/components/theme/index.js
@@ -6,7 +6,9 @@ const baseTheme = {
   },
 }
 
-const lightTheme = {
+export default baseTheme
+
+export const lightTheme = {
   ...baseTheme,
   colors: {
     background: '#fff',
@@ -17,7 +19,7 @@ const lightTheme = {
   },
 }
 
-const darkTheme = {
+export const darkTheme = {
   ...baseTheme,
   colors: {
     background: '#001933',
@@ -27,5 +29,3 @@ const darkTheme = {
     link: '#1ED3C6',
   },
 }
-
-export { lightTheme, darkTheme }
