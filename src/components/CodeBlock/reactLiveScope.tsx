f06f001f9281ed0eab4bceb12ef11ae80c10fe80
Josh Ellis <joshua.ellis18@gmail.com>
2021-04-13 22:21:15 +0100

    feat: add Imperatives & Refs

--- src/components/CodeBlock/reactLiveScope.tsx
+++ src/components/CodeBlock/reactLiveScope.tsx
@@ -1,5 +1,12 @@
 import { useRef, useEffect } from 'react'
-import { useSpring, animated, config } from '@react-spring/web'
+import {
+  useSpring,
+  animated,
+  config,
+  useChain,
+  useSpringRef,
+  useTransition,
+} from '@react-spring/web'
 import { mdx } from '@mdx-js/react'
 import useMeasure from 'react-use-measure'
 // import VisibilitySensor from 'react-visibility-sensor'
@@ -50,14 +57,33 @@ import useMeasure from 'react-use-measure'
 //   )
 // }
 
-export const scope = {
+const springScope = {
   useSpring,
+  useChain,
+  useSpringRef,
+  useTransition,
   config,
-  mdx,
-  useMeasure,
   animated,
-  POINTS:
-    '22.5 35.25 8.68704657 42.5118994 11.3250859 27.1309497 0.150171867 16.2381006 15.5935233 13.9940503 22.5 0 29.4064767 13.9940503 44.8498281 16.2381006 33.6749141 27.1309497 36.3129534 42.5118994',
+}
+
+const reactScope = {
   useRef,
   useEffect,
+}
+
+const utilityScope = {
+  mdx,
+  useMeasure,
+}
+
+const dataScope = {
+  POINTS:
+    '22.5 35.25 8.68704657 42.5118994 11.3250859 27.1309497 0.150171867 16.2381006 15.5935233 13.9940503 22.5 0 29.4064767 13.9940503 44.8498281 16.2381006 33.6749141 27.1309497 36.3129534 42.5118994',
+}
+
+export const scope = {
+  ...springScope,
+  ...reactScope,
+  ...utilityScope,
+  ...dataScope,
 }
